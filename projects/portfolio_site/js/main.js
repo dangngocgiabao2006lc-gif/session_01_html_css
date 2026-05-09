87fd3a09be7567fd9d9e6036db0471fe07f310c8
Dang Bao <dangngocgiabao2006lc@gmail.com>
2026-05-09 12:00:42 +0700

    [STYLE] Create about section layout

--- projects/portfolio_site/js/main.js
+++ projects/portfolio_site/js/main.js
@@ -1,1 +1,13 @@
- 
+ const skills = document.querySelectorAll(".skill-progress");
+
+function showSkills() {
+    skills.forEach(skill => {
+        const rect = skill.getBoundingClientRect();
+
+        if (rect.top < window.innerHeight) {
+            skill.style.width = skill.dataset.width;
+        }
+    });
+}
+
+window.addEventListener("scroll", showSkills);
